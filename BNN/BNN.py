4af7f36bea79e74ef2d58e55698030f1da8322cf
jillz-a <jilles@ziggo.nl>
2023-06-20 15:23:13 +0200

    simple model structure, going to add data

--- BNN/BNN.py
+++ BNN/BNN.py
@@ -1,5 +1,31 @@
 import os
+import numpy as np
 import torch
-from torch import nn
-from torch.utils.data import dataloader
+from torch import nn, save, load
+from torch.utils.data import DataLoader
+from torch.optim import Adam
 
+device = 'mps'
+#Frequentist neural network class
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(30*14, 5),
+            nn.ReLU(),
+            nn.Linear(5, 1),
+        )
+
+    def forward(self, x):
+        # x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+    
+#Model, optimizer and loss function
+NNmodel = NeuralNetwork().to(device)
+opt = Adam(NNmodel.parameters(), lr=1e-3)
+loss_fn = nn.CrossEntropyLoss()
+
+#training and validation data
+train = DataLoader()
