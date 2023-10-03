4519c3757e53e2b45b9ab93f6fdf13242d65b916
jillz-a <jilles@ziggo.nl>
2023-10-03 15:26:28 +0000

    problem is somewhere in the predict_fn function

--- DiCE/DiCE.py
+++ DiCE/DiCE.py
@@ -2,6 +2,8 @@
 #%% import dependencies
 import sys
 import os
+import shutil
+import importlib
 
 # Get the absolute path of the project directory
 project_path = os.path.dirname(os.path.abspath(os.path.join((__file__), os.pardir)))
@@ -20,18 +22,19 @@ import time
 from p_tqdm import p_map
 import tqdm
 
-import dice_ml_custom as dice_ml
-
 import warnings
 warnings.filterwarnings("ignore", category=pd.errors.PerformanceWarning)
+
+# import dice_ml_custom as dice_ml_custom
+
+
 
 from custom_BNN import CustomBayesianNeuralNetwork
 from custom_DNN import CustomNeuralNetwork
 
 
-
 # def load_required_packages():
-#     import dice_ml_custom as dice_ml
+#     import dice_ml_custom as dice_ml_custom
 #     from dice_ml_custom import Dice
 
 #     import numpy as np
@@ -53,9 +56,9 @@ with open(os.path.join(project_path, TESTDATASET, '0-Number_of_samples.csv')) as
 
 #Import into trained machine learning models
 if BayDet == 'BNN':
-    model = CustomBayesianNeuralNetwork().to(device)
+    model = CustomBayesianNeuralNetwork()
 elif BayDet == 'DNN':
-    model = CustomNeuralNetwork().to(device)
+    model = CustomNeuralNetwork()
     
 with open(f'{project_path}/BNN/model_states/{BayDet}_model_state_{DATASET}_test.pt', 'rb') as f: 
     model.load_state_dict(load(f)) 
@@ -66,7 +69,12 @@ cf_amount = 1
 #%%Go over each sample
 def CMAPSS_counterfactuals(file_path):
 
+
+
+    current_process = int(mp.current_process().name[15:])
+    dice_ml_custom = importlib.import_module('dice_ml_custom', os.path.join(project_path, f'dice_copies/dice_copy_{current_process}/dice_ml_custom'))
     
+
     #load sample with true RUL
     sample = np.genfromtxt(file_path, delimiter=" ", dtype=np.float32)
     sample_id = int(file_path[-13:-8])
@@ -86,14 +94,15 @@ def CMAPSS_counterfactuals(file_path):
     df_continuous_features = df.drop('RUL', axis=1).columns.tolist()
 
     #Data and model object for DiCE
-    data = dice_ml.Data(dataframe=df, continuous_features=df_continuous_features, outcome_name='RUL')
-    dice_model = dice_ml.Model(model=model, backend='PYT', model_type='regressor')
-    exp_random = dice_ml.Dice(data, dice_model, method='random')
+    data = dice_ml_custom.Data(dataframe=df, continuous_features=df_continuous_features, outcome_name='RUL')
+    dice_model = dice_ml_custom.Model(model=model, backend='PYT', model_type='regressor')
+    exp_random = dice_ml_custom.Dice(data, dice_model, method='random')
 
 
 
     #Generate counterfactual explanations
     cf_amount = 1
+    print(f'generating counterfactual {current_process}')
     cf = exp_random.generate_counterfactuals(df.drop('RUL', axis=1), 
                                              verbose=False, 
                                              total_CFs= cf_amount, 
@@ -105,6 +114,7 @@ def CMAPSS_counterfactuals(file_path):
     # cf.visualize_as_dataframe(show_only_changes=True)
     
     cf_total = cf.cf_examples_list[0].final_cfs_df
+    print(f'Counterfactual {current_process} processed')
     
     if cf_total is not None:
         #Save cf_result to file
@@ -130,6 +140,16 @@ if __name__ == '__main__':
 
     num_cores = mp.cpu_count()
 
+    #make copies of dice package folder to prevent GIL
+    source_folder = os.path.join(project_path, 'dice_ml_custom')
+    num_copies = num_cores
+    copy_folder = os.path.join(project_path, 'dice_copies')
+    if not os.path.exists(copy_folder): 
+        os.makedirs(copy_folder)
+        for i in range(num_copies):
+            copy_name = os.path.join(copy_folder, f'dice_copy_{i+1}/dice_ml_custom')
+            shutil.copytree(source_folder, copy_name)
+
     file_paths = glob.glob(os.path.join(project_path, TESTDATASET, '*.txt'))  # Get a list of all file paths in the folder
     file_paths.sort()
     file_paths = file_paths[0:int(sample_len[0][0])] #only looking at the first engine
@@ -146,5 +166,7 @@ if __name__ == '__main__':
     end = time.time()
     print('Processing ended')
     print('Time elapsed:', np.round((end-start)/60, 2), 'minutes')
+
+
 
     #%%
