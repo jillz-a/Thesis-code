6bef3cf2045cbc9469c788f5d4f3b1dca9c7e108
jillz-a <jilles@ziggo.nl>
2023-10-20 12:37:42 +0000

    added mp to DiCE_BNN and got results

--- DiCE/DiCE_BNN.py
+++ DiCE/DiCE_BNN.py
@@ -5,6 +5,11 @@ import os
 import csv
 import json
 
+os.environ["MKL_NUM_THREADS"] = "1" 
+os.environ["NUMEXPR_NUM_THREADS"] = "1" 
+os.environ["OMP_NUM_THREADS"] = "1" 
+os.environ['OPENBLAS_NUM_THREADS'] = '1'
+
 # Get the absolute path of the project directory
 project_path = os.path.dirname(os.path.abspath(os.path.join((__file__), os.pardir)))
 # Add the project directory to sys.path if it's not already present
@@ -13,13 +18,14 @@ if project_path not in sys.path:
 
 import numpy as np
 import torch
-from tqdm import tqdm
+import tqdm
 from sklearn.model_selection import KFold
 import pandas as pd
 
 from torch import nn, save, load
 from torchvision.transforms import ToTensor
 import matplotlib.pyplot as plt
+import multiprocessing as mp
 
 import bayesian_torch.layers as bl
 
@@ -83,23 +89,38 @@ def open_cf(file_path):
 
     return array, cf_RUL
 
+# Function to split a list into chunks
+def chunk_list(input_list, num_chunks):
+    avg_chunk_size = len(input_list) // num_chunks
+    remainder = len(input_list) % num_chunks
 
-if __name__ == "__main__":
-    SAVE = True
-    CF_DATASET = os.path.abspath(os.path.join(project_path, f'DiCE/BNN_cf_results/inputs/{DATASET}'))
+    chunks = []
+    start = 0
 
-    folder_path = f'data/{DATASET}/min-max/test'  # Specify the path to your folder
+    for i in range(num_chunks):
+        chunk_size = avg_chunk_size + (1 if i < remainder else 0)
+        end = start + chunk_size
+        chunks.append(input_list[start:end])
+        start = end
 
-    with open(os.path.join(project_path, folder_path, '0-Number_of_samples.csv')) as csvfile:
-        sample_len = list(csv.reader(csvfile)) #list containing the amount of samples per engine/trajectory
+    return chunks
 
+def CF_results(chunk):
     file_paths = glob.glob(os.path.join(CF_DATASET, '*.csv'))  # Get a list of all counterfactual input files
     file_paths.sort() 
 
-    RMSE_lst = []
+    engines = chunk
 
-    engines = np.arange(len(sample_len))
-    engines=[0]
+    # Model input parameters
+    input_size = 14 #number of features
+    hidden_size = 32
+    num_layers = 1
+
+    #Import into trained machine learning models
+    NNmodel = BayesianNeuralNetwork(input_size, hidden_size).to(device)
+    with open(f'{project_path}/BNN/model_states/BNN_model_state_{DATASET}_test.pt', 'rb') as f: 
+        NNmodel.load_state_dict(load(f)) 
+
     for engine in engines:
         index = sum([int(sample_len[0:i+1][i][0]) for i in range(engine)])
         selected_file_paths = file_paths[index:index + int(sample_len[engine][0])]  # Select the desired number of files
@@ -110,23 +131,14 @@ if __name__ == "__main__":
         true_lst = []
         var_pred_lst = []
 
-        # Model input parameters
-        input_size = 14 #number of features
-        hidden_size = 32
-        num_layers = 1
 
         #Go through each sample
-        loop = tqdm(selected_file_paths)
-        for file_path in loop:
+        for file_path in selected_file_paths:
         
             # Process each selected file
             sample, cf_RUL = open_cf(file_path)
             label = float(file_path[-7:-4])
 
-            #Import into trained machine learning models
-            NNmodel = BayesianNeuralNetwork(input_size, hidden_size).to(device)
-            with open(f'{project_path}/BNN/model_states/BNN_model_state_{DATASET}_test.pt', 'rb') as f: 
-                NNmodel.load_state_dict(load(f)) 
 
             #predict RUL from samples using Monte Carlo Sampling
             X = ToTensor()(sample).to(device)
@@ -145,12 +157,9 @@ if __name__ == "__main__":
             mean_pred_lst.append(mean_pred.item())
             true_lst.append(y)
             var_pred_lst.append(var_pred.item())
-            
-            loop.set_description(f"Processing engine {engine}")
 
         error = [(mean_pred_lst[i] - true_lst[i])**2 for i in range(len(true_lst))] #squared BNN error
         B_RMSE = np.round(np.sqrt(np.mean(error)), 2) #Root Mean Squared error of Bayesian prediciton
-        RMSE_lst.append(B_RMSE)
 
         #save engine results to file
         if SAVE:
@@ -168,5 +177,24 @@ if __name__ == "__main__":
             with open(file_name, 'w') as jsonfile:
                 json.dump(results, jsonfile)
 
-    print(f'Evaluation completed for dataset {DATASET}')
-    print(f'Bayesian Neural Network RMSE for {len(engines)} engines = {np.mean(RMSE_lst)} cycles')
+
+
+if __name__ == "__main__":
+    SAVE = True
+    CF_DATASET = os.path.abspath(os.path.join(project_path, f'DiCE/BNN_cf_results/inputs/{DATASET}'))
+
+    folder_path = f'data/{DATASET}/min-max/test'  # Specify the path to your folder
+
+    num_cores = mp.cpu_count()
+
+    with open(os.path.join(project_path, folder_path, '0-Number_of_samples.csv')) as csvfile:
+        sample_len = list(csv.reader(csvfile)) #list containing the amount of samples per engine/trajectory
+
+
+    engines = np.arange(len(sample_len))
+
+    chunks = chunk_list(engines, min(num_cores, len(engines)))
+
+
+    with mp.Pool(processes=min(num_cores, len(chunks))) as pool:
+        list(tqdm.tqdm(pool.imap_unordered(CF_results, chunks), total=len(chunks)))
